74414066440bd6238142925048604525e3f7b1ec
akash0997 <akashr0509@gmail.com>
2020-12-07 03:47:32 +0530

    changes in groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,5 +3,10 @@ def call (docker_user)
     println (docker_user)
     def script_content = libraryResource 'dockerBuild.sh'
     writeFile file: './dockerBuild.sh ', text: script_content
-    sh 'pwd && ls -al && bash ./dockerBuild.sh "$docker_user"'
+    sh '''
+    pwd
+    chmod +x ./dockerBuild.sh
+    ls -al
+    bash ./dockerBuild.sh "$docker_user"
+    '''
 }
