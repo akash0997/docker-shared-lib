62f5d9a5f45a45740739b47b26e4840e7f8d0960
akash0997 <akashr0509@gmail.com>
2020-12-07 02:52:54 +0530

    changed shell script

--- resources/dockerDeploy.sh
+++ resources/dockerDeploy.sh
@@ -2,4 +2,4 @@
 container_id=$(docker ps -qf "name=${JOB_NAME}") && [ -z $container_id ] && echo "Container not running" || (docker stop $container_id && docker rm $container_id)
 docker run -p 5000:8000 --name ${JOB_NAME} -d akash97/${JOB_NAME}:latest
 docker ps | grep ${JOB_NAME}
-docker logs
+docker logs ${JOB_NAME}
