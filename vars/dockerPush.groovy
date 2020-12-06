3a1089409bc07d61520c2b607ed28c058e456add
akash0997 <akashr0509@gmail.com>
2020-12-07 01:30:36 +0530

    sixth

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,8 +1,13 @@
-def call (user,pass)
-{
-   sh '''
-      docker login --username ${user} --password ${pass}
-      docker push akash97/${JOB_NAME}:${BUILD_NUMBER}
-      docker push akash97/${JOB_NAME}:latest
-     '''
+def call() {
+    withCredentials([usernamePassword(
+            credentialsId: "docker-cred",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+       sh '''
+                docker login --username ${USER} --password ${PASS}
+                docker push akash97/${JOB_NAME}:${BUILD_NUMBER}
+                docker push akash97/${JOB_NAME}:latest
+            '''
+    }
 }
