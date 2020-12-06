adb3f400d3abd2bd9a6b2cc0dcc80bd7a2949889
akash0997 <akashr0509@gmail.com>
2020-12-07 04:24:07 +0530

    changes in groovy

--- resources/dockerBuild.sh
+++ resources/dockerBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker_user=$1
-echo '$docker_user'
+echo "$docker_user"
 docker build -t $docker_user/${JOB_NAME}:${BUILD_NUMBER} .
 docker tag $docker_user/${JOB_NAME}:${BUILD_NUMBER} $docker_user/${JOB_NAME}:latest
 docker images
