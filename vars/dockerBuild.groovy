c416df57998e02808f6406ef690b1702d6abd406
akash0997 <akashr0509@gmail.com>
2020-12-07 02:40:26 +0530

    eighth

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,6 @@
 def call ()
 {
-    def scriptDir = WORKSPACE + '@libs/shared-lib'
-    sh "$scriptDir/dockerBuild.sh"
-//    sh '''
-//     pwd
-//     docker build -t akash97/${JOB_NAME}:${BUILD_NUMBER} .
-//     docker tag akash97/${JOB_NAME}:${BUILD_NUMBER} akash97/${JOB_NAME}:latest
-//     docker images
-//     '''
+    def script_content = libraryResource 'dockerBuild.sh'
+    writeFile file: './dockerBuild.sh', text: script_content
+    sh 'bash ./test.sh'
 }
--- vars/dockerBuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pwd
-docker build -t akash97/${JOB_NAME}:${BUILD_NUMBER} .
-docker tag akash97/${JOB_NAME}:${BUILD_NUMBER} akash97/${JOB_NAME}:latest
-docker images
