b69a7f6ad15c37d5376e8faa7c4c81e6a358a73f
akash0997 <akashr0509@gmail.com>
2020-12-07 04:46:54 +0530

    changes in groovy

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -2,5 +2,7 @@ def call (docker_user,external_port,internal_port)
 {
     def script_content = libraryResource 'dockerDeploy.sh'
     writeFile file: './dockerDeploy.sh', text: script_content
-    sh 'bash ./dockerDeploy.sh $docker_user $external_port $internal_port'   
+    sh """
+    bash dockerDeploy.sh $docker_user $external_port $internal_port
+    """  
 }
