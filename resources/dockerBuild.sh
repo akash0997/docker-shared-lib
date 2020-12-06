b17104f67e26d522f0b8e2b8460618e21317a319
akash0997 <akashr0509@gmail.com>
2020-12-07 04:16:13 +0530

    changes in groovy

--- resources/dockerBuild.sh
+++ resources/dockerBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
-docker_user = $1
+docker_user=$1
 docker build -t $docker_user/${JOB_NAME}:${BUILD_NUMBER} .
 docker tag $docker_user/${JOB_NAME}:${BUILD_NUMBER} $docker_user/${JOB_NAME}:latest
 docker images
--- resources/dockerDeploy.sh
+++ resources/dockerDeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker_user = $1
-external_port = $2
-internal_port = $3
+docker_user=$1
+external_port=$2
+internal_port=$3
 container_id=$(docker ps -qf "name=${JOB_NAME}") && [ -z $container_id ] && echo "Container not running" || (docker stop $container_id && docker rm $container_id)
 docker run -p $external_port:$internal_port --name ${JOB_NAME} -d $docker_user/${JOB_NAME}:latest
 docker ps | grep ${JOB_NAME}
