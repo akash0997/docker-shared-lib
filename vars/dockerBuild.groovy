7363a906f7753c3e310644035a6f0c00614d389f
akash0997 <akashr0509@gmail.com>
2020-12-07 04:13:39 +0530

    changes in groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,5 +3,5 @@ def call (docker_user)
     println (docker_user)
     def script_content = libraryResource 'dockerBuild.sh'
     writeFile file: './dockerBuild.sh', text: script_content
-    sh 'cat dockerBuild.sh && bash ./dockerBuild.sh'
+    sh 'bash ./dockerBuild.sh $docker_user'
 }
